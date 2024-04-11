7f63529de7e32f94fc64bba3408c0a6a7208ebe8
Egor Oparin <gregorop@yandex.ru>
2024-04-11 20:13:44 +0300

    создана структура

--- first.py
+++ first.py
@@ -1,5 +1,13 @@
 #тут будет класс с окном - инстукцией
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 class First():
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.win = QWidget()
+        self.win.show()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    win1 = First()
+    app.exec()
